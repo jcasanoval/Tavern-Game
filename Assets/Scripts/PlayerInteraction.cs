4720e3ccea101a0b675f9c6088a30a72ce6c250f
Juan Casanova <j.casanoval@hotmail.com>
2024-09-13 17:39:11 -0300

    fix: prevent interaction deadlocks

--- Assets/Scripts/PlayerInteraction.cs
+++ Assets/Scripts/PlayerInteraction.cs
@@ -1,24 +1,33 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerInteraction : MonoBehaviour
 {
-    private Interactable currentInteractable;
+    private Interactable currentInteractable
+    {
+        get
+        {
+            return nearbyInteractables.Count > 0 ? nearbyInteractables[0] : null;
+        }
+    }
+
+    private List<Interactable> nearbyInteractables = new List<Interactable>();
 
     private void OnTriggerEnter(Collider other)
     {
         Interactable interactable = other.GetComponent<Interactable>();
         if (interactable != null)
         {
-            currentInteractable = interactable;
+            nearbyInteractables.Add(interactable);
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
         Interactable interactable = other.GetComponent<Interactable>();
-        if (interactable != null && interactable == currentInteractable)
+        if (interactable != null)
         {
-            currentInteractable = null;
+            nearbyInteractables.Remove(interactable);
         }
     }
 
