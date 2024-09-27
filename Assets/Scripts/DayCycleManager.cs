d8f009908f72dac2d343353521fa09b6f241a83d
GiulianoRositto <giulianorositto@gmail.com>
2024-09-27 03:05:58 -0300

    Doors and Drinks

--- Assets/Scripts/DayCycleManager.cs
+++ Assets/Scripts/DayCycleManager.cs
@@ -10,6 +10,8 @@ public class DayCycleManager : MonoBehaviour
     private AudioSource doorCloseAudioSource;
     private AudioSource[] audioSources;
 
+    public Transform door;
+
     void Awake()
     {
         audioSources = GetComponents<AudioSource>();
@@ -48,6 +50,9 @@ public class DayCycleManager : MonoBehaviour
             yield return null;
         }
 
+
+        door.position = new Vector3(6.02080011f,0.753099978f,-4.6262002f);
+        door.rotation = Quaternion.Euler(0, 0, 0);
         backgroundNoiseAudioSource.Stop();
         doorCloseAudioSource.Play();
     }
--- Assets/Scripts/DoorInteractable.cs
+++ Assets/Scripts/DoorInteractable.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DoorInteractable : Interactable
 {
     private DayCycleManager dayCycleManager;
     private AudioSource doorOpenAudioSource;
+
+    public Transform changed;
 
     void Awake()
     {
@@ -16,21 +19,14 @@ public class DoorInteractable : Interactable
     {
         dayCycleManager = FindObjectOfType<DayCycleManager>();
     }
-
-    void Update() {
-        if (dayCycleManager.IsOpen()) {
-            GetComponentsInParent<Renderer>()[0].material.color = Color.green;
-        }
-        else {
-            GetComponentsInParent<Renderer>()[0].material.color = Color.red;
-        }
-    }
-
     public override bool Interact()
     {
+
         if (!dayCycleManager.IsOpen()) {
             dayCycleManager.Open();
             doorOpenAudioSource.Play();
+            changed.position = new Vector3(5.25f,0.753099978f,-5.5f);
+            changed.rotation = Quaternion.Euler(0, -90, 0);
             return true;
         }
         return false;
