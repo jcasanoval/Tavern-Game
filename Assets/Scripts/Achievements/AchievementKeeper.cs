1b52e9589c57f725552d5e7177181968e06d4abd
NicoTorres25 <nicolastorres250@gmail.com>
2024-11-27 17:01:54 -0300

    fix: delete unnecessary imports that break build

--- Assets/Scripts/Achievements/AchievementKeeper.cs
+++ Assets/Scripts/Achievements/AchievementKeeper.cs
@@ -1,8 +1,4 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.InteropServices;
-using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
 
 public class AchievementKeeper : MonoBehaviour
