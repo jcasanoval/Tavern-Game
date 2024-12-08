dabb8fd7eba7be8271b60579dfc056848bb485ce
GiulianoRositto <giulianorositto@gmail.com>
2024-12-08 16:45:18 -0300

    Better File Distribution

---
+++ Assets/Scripts/Achievements/DataKeeping/LedgerRecord.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LedgerRecord{
+    public float BeersSold { get; set; }
+    public float Night { get; set; }
+    public float Customer_Left_Angry { get; set; }
+
+    public float Money_Earned { get; set; }
+
+    public float Money_Spent { get; set; }
+
+    public float Total_Customers { get; set; }
+
+    public float Popularity { get; set; }
+
+}
